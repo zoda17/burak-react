5a20c15167d219af120bfb487a0f42e4f121dfd9
zoda17 <shakhnoza.kenjaeva1998@gmail.com>
2024-10-03 19:52:03 +0900

    BRR: Initial setup

--- src/index 2.tsx
+++ src/index 2.tsx
@@ -1,26 +1,20 @@
 import React from 'react';
-import  ReactDOM  from "react-dom";
+import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { store } from './app/store';
-import App from './app/App';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
-import  CssBaseline  from '@mui/material/CssBaseline';
-import { ThemeProvider } from '@mui/material/styles';
-import './css/index.css';
-import theme from './app/MaterialTheme';
+import './index.css';
 
+const container = document.getElementById('root')!;
+const root = createRoot(container);
 
-
-ReactDOM.render(
+root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <ThemeProvider theme={theme}> 
-      <CssBaseline />
       <App />
-      </ThemeProvider>
     </Provider>
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
