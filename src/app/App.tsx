0cdd362bf1f8a9a58932813976cc28cc5a064013
zoda17 <shakhnoza.kenjaeva1998@gmail.com>
2024-10-07 23:02:24 +0900

    feat: develop header and footer part one

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import '../css/app.css';
 import { Box, Button, Container, Stack, Typography } from '@mui/material';
 import { Link, Route, Switch, useLocation } from 'react-router-dom';
 import { HomePage } from './screens/homepage';
@@ -9,6 +8,9 @@ import { UserPage } from './screens/userPage';
 import { HomeNavbar } from './components/headers/HomeNavbar';
 import { OtherNavbar } from './components/headers/OtherNavbar';
 import { Footer } from './components/footer';
+import '../css/app.css';
+import "../css/navbar.css"
+import { HelpPage } from './screens/helpPage';
 
 function App() {
   const location = useLocation();
@@ -24,6 +26,9 @@ function App() {
     </Route>
     <Route path="/member-page">
       <UserPage />
+    </Route>
+    <Route path="/help">
+      <HelpPage />
     </Route>
     <Route path="/">
       <HomePage />
