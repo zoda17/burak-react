4b319bd549e6ccb49c136008850f7e9b906a09eb
zoda17 <shakhnoza.kenjaeva1998@gmail.com>
2024-10-08 00:41:12 +0900

    feat: publish otherNavbar header component

--- src/app/components/headers/OtherNavbar.tsx
+++ src/app/components/headers/OtherNavbar.tsx
@@ -1,3 +1,54 @@
+import { Box, Button, Container, Stack } from "@mui/material";
+import { NavLink } from "react-router-dom";
+
 export function OtherNavbar() {
-    return <div>OtherNavbar</div>;
+    const authMember = null;
+    return (<div className="other-navbar">
+    <Container className="navbar-container">
+        <Stack className="menu">
+        <Box>
+            <NavLink to="/">
+                <img className="brand-logo" 
+                src="/icons/burak.svg"/>
+            </NavLink>
+        </Box>
+        <Stack className="links"
+        >
+        <Box className={"hover-line"}>
+            <NavLink to="/">Home</NavLink>
+        </Box>
+        <Box className={"hover-line"}>
+            <NavLink to="/products" activeClassName={"underline"}>Products</NavLink>
+        </Box>
+        {authMember ? ( 
+        <Box className={"hover-line"}>
+            <NavLink to="/orders" activeClassName={"underline"}>Orders</NavLink>
+        </Box>) : null}
+        {authMember ? ( 
+        <Box className={"hover-line"}>
+            <NavLink to="/member-page" activeClassName={"underline"}>My Page</NavLink>
+        </Box>) : null}
+        <Box className={"hover-line"}>
+            <NavLink to="/help" activeClassName={"underline"}>Help</NavLink>
+        </Box>
+        {/* BASKET */}
+
+        {!authMember ? (
+        <Box>
+            <Button 
+            variant="contained" 
+            className="login-button">Login</Button>
+            </Box>
+        ) : (
+        <img 
+        className="user-avatar"
+        src={"/icons/default-user.svg"}
+        aria-haspopup={"true"}
+        />
+        )}
+        </Stack>
+        </Stack>
+    </Container>
+</div>
+);
 }
