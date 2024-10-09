b93d8f37039f62fece87b736f6ada1dfc36b3b2d
zoda17 <shakhnoza.kenjaeva1998@gmail.com>
2024-10-10 07:09:31 +0900

    feat:active users sectional components

--- src/app/screens/homepage/ActiveUsers.tsx
+++ src/app/screens/homepage/ActiveUsers.tsx
@@ -1,10 +1,53 @@
-import React from "react";
-import { Container } from "@mui/material";
+import { Box, Container, Stack } from "@mui/material";
+import { CssVarsProvider } from "@mui/joy/styles";
+import Card from "@mui/joy/Card";
+import CardOverflow from "@mui/joy/CardOverflow";
+import Typography from "@mui/joy/Typography";
+import AspectRatio from "@mui/joy/AspectRatio";
 
-export default function ActiveUsers() {
-    return (
-    <div>
-        <Container>ActiveUsers</Container>
+const userList = [
+  { productName: "Martin", imagePath: "/img/martin.webp" },
+  { productName: "Justin", imagePath: "/img/justin.webp" },
+  { productName: "Rose", imagePath: "/img/rose.webp" },
+  { productName: "Nusret", imagePath: "/img/nusret.webp" },
+];
+
+function ActiveUsers() {
+  return (
+    <div className={"active-users-frame"}>
+      <Container>
+        <Stack className={"main"}>
+          <Box className={"category-title"}>Active Users</Box>
+          <Stack className={"cards-frame"}>
+            <CssVarsProvider>
+              {userList.length !== 0 ? (
+                userList.map((ele, index) => {
+                  return (
+                    <Card key={index} variant="outlined" className={"card"}>
+                      <CardOverflow>
+                        <AspectRatio ratio="1">
+                            <img src={ele.imagePath} alt="image" />
+                        </AspectRatio>
+                      </CardOverflow>
+                      <CardOverflow variant="soft" className="product-detail">
+                        <Stack className="info">
+                            <Typography className={"name"}>
+                                {ele.productName}
+                            </Typography>
+                        </Stack>
+                      </CardOverflow>
+                    </Card>
+                  );
+                })
+              ) : (
+                <Box className="no-data">New Active Users are not available!</Box>
+              )}
+            </CssVarsProvider>
+          </Stack>
+        </Stack>
+      </Container>
     </div>
-    );
+  );
 }
+
+export default ActiveUsers;
