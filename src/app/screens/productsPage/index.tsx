ab36f660096657b990cbe9d6bdd60a3159793652
zoda17 <shakhnoza.kenjaeva1998@gmail.com>
2024-10-12 11:05:28 +0900

    feat: products page screen component

--- src/app/screens/productsPage/index.tsx
+++ src/app/screens/productsPage/index.tsx
@@ -1,5 +1,21 @@
+import React from "react";
+import { Route, Switch, useRouteMatch } from "react-router-dom";
 import { Container } from "@mui/material";
+import ChosenProduct from "./ChosenProduct";
+import Product from "./Products";
+import Products from "./Products";
 
 export default function ProductsPage() {
-    return <Container>ProductsPage</Container>;
-  }
+    const products = useRouteMatch();
+    console.log("products:", products);
+    return <div className={"products-page"}>
+        <Switch>
+            <Route path={`${products.path}/:productId`}>
+             <ChosenProduct />
+            </Route>
+            <Route path={`${products.path}`}>
+             <Products />
+            </Route>
+        </Switch>
+    </div>;
+}
